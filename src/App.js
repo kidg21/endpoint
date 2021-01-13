45cc3175921658feaf22edcfa05e0669e2547fa6
kidg21 <kidg21@yahoo.com>
2021-01-12 19:40:27 -0800

    Updates to App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import GlobalStyles from './base/GlobalStyles.jsx'
+// import Grid from './components/Grid.jsx';
+import Card, { CardGrid } from './components/Card.jsx';
+import Image from './components/Image.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <React.Fragment>
+      <GlobalStyles />
+      <CardGrid>
+        <Card
+          id="Card_1"
+          media="https://i.picsum.photos/id/1005/5760/3840.jpg?hmac=2acSJCOwz9q_dKtDZdSB-OIK1HUcwBeXco_RMMTUgfY"
+          title="This is a Title"
+          subtitle="This is a Subtitle"
+          onClick={() => { }}
+        />
+        <Card
+          id="Card_2"
+          media="https://i.picsum.photos/id/1011/5472/3648.jpg?hmac=Koo9845x2akkVzVFX3xxAc9BCkeGYA9VRVfLE4f0Zzk"
+          title="This is a Title"
+          subtitle="This is a Subtitle"
+          onClick={() => { }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {/* <Image src="https://i.picsum.photos/id/1011/5472/3648.jpg?hmac=Koo9845x2akkVzVFX3xxAc9BCkeGYA9VRVfLE4f0Zzk" /> */}
+        </Card>
+        <Card
+          id="Card_3"
+          media="https://i.picsum.photos/id/1027/2848/4272.jpg?hmac=EAR-f6uEqI1iZJjB6-NzoZTnmaX0oI0th3z8Y78UpKM"
+          title="This is a Title"
+          subtitle="This is a Subtitle"
+          onClick={() => { }}
+        />
+        <Card
+          id="Card_4"
+          media="https://i.picsum.photos/id/1040/4496/3000.jpg?hmac=kvZONlBpTcZ16PuE_g2RWxlicQ5JKVq2lqqZndfafBY"
+          title="This is a Title"
+          subtitle="This is a Subtitle"
+          onClick={() => { }}
+        />
+      </CardGrid>
+    </React.Fragment>
   );
 }
 
